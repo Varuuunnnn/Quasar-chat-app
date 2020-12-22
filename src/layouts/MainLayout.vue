c85265cee29cfa16e910adeb076c0bf706252842
Varun <varungurjar@cilalabs.com>
2020-12-23 00:04:34 +0530

    add files via upload

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          v-if="$route.fullPath.includes('/chat')"
+          v-go-back="'/'"
+         
+          icon="arrow_back"
+          flat
+          dense
+          label="Back"/>
+       
+
+        <q-toolbar-title class="absolute-center">
+          {{ title }}
+        </q-toolbar-title>
+
+        
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+export default {
+  computed:{
+   title(){
+     let currentPath=this.$route.fullPath
+     if(currentPath=='/') return 'SmackChat'
+     else if(currentPath=='/chat') return 'Chat'
+     else if(currentPath=='/auth') return 'Login'
+   }
+  },
+  name: 'MainLayout',
+  components: { EssentialLink },
+  
+  
+}
+</script>
---
+++ src/pages/PageChat.vue
@@ -0,0 +1,69 @@
+<template>
+  <q-page class="flex column">
+    <div class="q-pa-md column col justify-end">
+      <q-chat-message
+         v-for="message in messages"
+         :key="message.text"
+         :name="message.from"
+         :text="[message.text]"
+         :sent="message.from == 'me'? true : false"
+      />
+    </div>
+     <q-footer elevated>
+        <q-toolbar>
+         <q-form @submit="sendMessage" class="full-width">
+           <q-input
+              v-model="newMessage"
+              bg-color="white"
+              rounded
+              outlined
+              label="message"
+              dense>
+            
+            <template v-slot:after>
+              <q-btn
+                round
+                dense
+                flat
+                type="submit"
+                color="white"
+                icon="send" />
+            </template>
+           </q-input>  
+         </q-form> 
+        </q-toolbar>
+      </q-footer>
+  </q-page>
+</template>
+
+<script>
+export default {
+ data(){
+   return{
+     newMessage:'',
+     messages:[
+       {
+         text:'Hey abhi,how are you man?',
+         from:'me'
+       },
+       {
+         text:'I am doing good',
+         from:'them'
+       },
+       {
+         text:'pretty cool',
+         from:'me'
+       },
+     ]
+   }
+ },
+ methods: {
+   sendMessage(){
+     this.messages.push({
+       text: this.newMessage,
+       from:'me'
+     })
+   }
+ }
+}
+</script>
